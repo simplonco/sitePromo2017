16285726c663fde56f4a96209bc3c637e52697cd
lou nunkabuk <laureloobagatelle@hotmail.fr>
2017-03-24 14:40:36 +0100

    modif trombi php

--- trombi.php
+++ trombi.php
@@ -46,22 +46,8 @@
             <div id="loading">Please wait...</div>
         </div>
     </div>
-  <footer>
-    <div class="partners">
-      <h2>NOS PARTENAIRES :</h2>
-    </div>
-    <div class="logoPartners">
-      <a href="http://www.legrandnarbonne.com/"> <img src="assets/img/logo-grand-narbonne.png" alt="logo grand narbonne"> </a>
-      <a href="http://simplon.co/"> <img src="assets/img/logo-simplon.jpg" alt="logo simplon"> </a>
-      <a href="http://entreprendre.legrandnarbonne.com/717-iness-des-hommes-des-projets.html"> <img class="logoIness" src="assets/img/img_INESS.jpg" alt="logo in'ess"> </a>
-      <a href="http://www.laregion.fr/"> <img src="assets/img/OC-1702-instit-logo carre-RVB-150x150-300dpi.jpg" alt="logo région occitanie"> </a>
-      <a href="http://www.nucleum.fr/profile/starlancerstudio-2/"> <img src="assets/img/starlancer.jpg" alt="logo starlancer studios"> </a>
-    </div>
-    <div class="copyright">
-      <p>&copy; Réalisé par Simplon Narbonne Promo #2 - 2017 </p>
-      <a href="#haut"> <img class="fleche" src="assets/img/fleche.png" alt="image fleche"> </a>
-    </div>
-  </footer>
+<?php include 'footer.php';?>
+
 
 <script src="assets/js/trombi.js"></script>
 <script src="assets/jquery.jswipe-0.1.2.js"></script>
