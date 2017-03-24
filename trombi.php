fd33d9cf8b840c88fb6b2651fb4b0b04e63d44fe
Eloïse Mécozzi <eloisemecozzi@gmail.com>
2017-03-24 14:30:45 +0100

    ajout trombi php

---
+++ trombi.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="fr">
+<meta charset="utf-8">
+<head>
+
+<title>Trombinoscope Simplon Narbonne 2017</title>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<link rel="stylesheet" href="assets/css/trombi.css">
+
+<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="assets/jquery.jswipe-0.1.2.js"></script>
+
+</head>
+<body>
+    <div class="content">
+      <header class="header">
+        <div class="navBar">
+          <ul class="topnav">
+            <li><a href="./contact.html"> Contact </a></li>
+            <li><a href="#"> A propos </a></li>
+            <li><a href="./index.html"> Accueil </a></li>
+          </ul>
+        </div>
+    </header>
+
+        <div class="trombi">
+            <div id="gallery">
+                <img src="assets/img/fred.png" alt="Fred : Sic parvis magna" aria-linkedin="" aria-github="https://github.com/anneserrano"/>
+                <img src="assets/img/bruno.png" alt="Bruno : Je réfléchis." aria-linkedin="" aria-github="https://github.com/anneserrano"/>
+                <img src="assets/img/memilie.png" alt="Mémilie : Styleyyyy" aria-linkedin="" aria-github="https://github.com/anneserrano"/>
+                <img src="assets/img/sandra.png" alt="Sandra : C'est pénible" aria-linkedin="" aria-github="https://github.com/anneserrano"/>
+                <img src="assets/img/romain.png" alt="Romain : Ouais" aria-linkedin="" aria-github="https://github.com/anneserrano"/>
+                <img src="assets/img/axel.png" alt="Axel : C'est trop facile" aria-linkedin="" aria-github="https://github.com/anneserrano"/>
+                <img src="assets/img/mobylette.png" alt="Mobylette : Vive le vélo" aria-linkedin="" aria-github="https://github.com/anneserrano"/>
+                <img src="assets/img/anne.png" alt="Anne : Le thé c'est la vie" aria-linkedin="" aria-github="https://github.com/anneserrano"/>
+                <img src="assets/img/ivan.png" alt="Ivan" aria-linkedin="" aria-github="https://github.com/anneserrano"/>
+                <img src="assets/img/aida.png" alt="Aïda" aria-linkedin="" aria-github="https://github.com/anneserrano"/>
+                <img src="assets/img/hermeline.png" alt="Hermeline" aria-linkedin="" aria-github="https://github.com/anneserrano"/>
+                <img src="assets/img/marl.png" alt="Marlène : Je te surveille" aria-linkedin="" aria-github="https://github.com/anneserrano"/>
+                <img src="assets/img/jenny.png" alt="Jenny : Girl power" aria-linkedin="" aria-github="https://github.com/anneserrano"/>
+                <img src="assets/img/lou.png" alt="Lou : ÇA MARCHE PAAAAAAS !!!!!" aria-linkedin="" aria-github="https://github.com/anneserrano"/>
+                <img src="assets/img/elo.png" alt="Elo" aria-linkedin="" aria-github="https://github.com/anneserrano"/>
+                <img src="assets/img/pat.png" alt="Patrick : Raspberry power" aria-linkedin="" aria-github="https://github.com/anneserrano"/>
+                <img src="assets/img/nicoav.png" alt="Nico : On va faire un exercice marrant" aria-linkedin="" aria-github="https://github.com/anneserrano"/>
+            </div>
+
+                <input type="range" name="slider" id="slider" />
+            <div id="caption"></div>
+            <div id="loading">Please wait...</div>
+        </div>
+    </div>
+
+  <footer>
+    <div class="partners">
+      <h2>NOS PARTENAIRES :</h2>
+    </div>
+    <div class="logoPartners">
+      <a href="http://www.legrandnarbonne.com/"> <img src="assets/img/logo-grand-narbonne.png" alt="logo grand narbonne"> </a>
+      <a href="http://simplon.co/"> <img src="assets/img/logo-simplon.jpg" alt="logo simplon"> </a>
+      <a href="http://entreprendre.legrandnarbonne.com/717-iness-des-hommes-des-projets.html"> <img class="logoIness" src="assets/img/img_INESS.jpg" alt="logo in'ess"> </a>
+      <a href="http://www.laregion.fr/"> <img src="assets/img/OC-1702-instit-logo carre-RVB-150x150-300dpi.jpg" alt="logo région occitanie"> </a>
+      <a href="http://www.nucleum.fr/profile/starlancerstudio-2/"> <img src="assets/img/starlancer.jpg" alt="logo starlancer studios"> </a>
+    </div>
+    <div class="copyright">
+      <p>&copy; Réalisé par Simplon Narbonne Promo #2 - 2017 </p>
+      <a href="#haut"> <img class="fleche" src="assets/img/fleche.png" alt="image fleche"> </a>
+    </div>
+  </footer>
+
+<script src="assets/js/trombi.js"></script>
+<script src="assets/jquery.jswipe-0.1.2.js"></script>
+</body>
+</html>
