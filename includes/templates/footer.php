6e1ff3ba89f86df9461f588a9b0d038866cf486b
bibouille <bibouille66@gmail.com>
2017-03-31 11:32:05 +0200

    footer

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,27 +1,16 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" href="./assets/css/footer.css">
-
-  </head>
-  <body>
     <footer>
     <div class="partners">
       <h2>NOS PARTENAIRES :</h2>
     </div>
     <div class="logoPartners">
-      <a href="http://www.legrandnarbonne.com/"> <img src="./assets/img/logo-grand-narbonne.png" alt="logo grand narbonne"> </a>
-      <a href="http://simplon.co/"> <img src="./assets/img/logo-simplon.jpg" alt="logo simplon"> </a>
-      <a href="http://entreprendre.legrandnarbonne.com/717-iness-des-hommes-des-projets.html"> <img class="logoIness" src="./assets/img/img_INESS.jpg" alt="logo in'ess"> </a>
-      <a href="http://www.laregion.fr/"> <img src="./assets/img/OC-1702-instit-logo carre-RVB-150x150-300dpi.jpg" alt="logo région occitanie"> </a>
-      <a href="http://www.nucleum.fr/profile/starlancerstudio-2/"> <img src="./assets/img/starlancer.jpg" alt="logo starlancer studios"> </a>
+      <a href="http://www.legrandnarbonne.com/" target="_blank"> <img src="./assets/img/logo-grand-narbonne.png" alt="logo grand narbonne"> </a>
+      <a href="http://simplon.co/" target="_blank"> <img src="./assets/img/logo-simplon.jpg" alt="logo simplon"> </a>
+      <a href="http://entreprendre.legrandnarbonne.com/717-iness-des-hommes-des-projets.html" target="_blank"> <img class="logoIness" src="./assets/img/img_INESS.jpg" alt="logo in'ess"> </a>
+      <a href="http://www.laregion.fr/" target="_blank"> <img src="./assets/img/logo-occitanie.jpg" alt="logo région occitanie"> </a>
+      <a href="http://www.nucleum.fr/profile/starlancerstudio-2/" target="_blank"> <img src="./assets/img/starlancer.jpg" alt="logo starlancer studios"> </a>
     </div>
     <div class="copyright">
       <p>&copy; Réalisé par Simplon Narbonne Promo #2 - 2017 </p>
       <a href="#haut"> <img class="fleche" src="./assets/img/fleche.png" alt="image fleche"> </a>
     </div>
   </footer>
-  </body>
-</html>
