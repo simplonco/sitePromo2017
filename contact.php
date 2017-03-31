11bc83e3c0db77b27c7c87ec7cd58b23eec17730
Sam <baleclyps@hotmail.fr>
2017-03-31 11:22:56 +0200

    modif map

--- contact.php
+++ contact.php
@@ -58,7 +58,7 @@
 </div> -->
 
  <div class="map">
-<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m11!1m3!1d20384.295043716134!2d2.969329854056524!3d43.18003977879723!2m2!1f0!2f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b1ac52defc6925%3A0xb10f76c5fd1d874a!2sIN&#39;ESS+Le+Grand+Narbonne!5e1!3m2!1sfr!2sfr!4v1488979146577" width="600" height="450" frameborder="0" style="border:5" allowfullscreen></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m11!1m3!1d20384.295043716134!2d2.969329854056524!3d43.18003977879723!2m2!1f0!2f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b1ac52defc6925%3A0xb10f76c5fd1d874a!2sIN&#39;ESS+Le+Grand+Narbonne!5e1!3m2!1sfr!2sfr!4v1488979146577" width="600" height="450" style="border:5" allowfullscreen></iframe>
 </div>
 
 <?php include("includes/templates/footer.php"); ?>
