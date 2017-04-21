838d9da11f8867c587849290dff5d6c82454454d
anneserrano <anneserrano@localhost.localdomain>
2017-04-21 09:59:08 +0200

    nouveau fichier

---
+++ a_propos.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Présentation</title>
+</head>
+<body>
+  <?php include 'includes/templates/header.php';?>
+  <h1> A propos... </h1>
+
+  <h2> SIMPLON</h2>
+
+  <p>Entreprise de l’économie sociale et solidaire (agrément ESUS), Simplon.co est un réseau de « fabriques » (écoles) qui propose des formations GRATUITES pour devenir développeur de sites web et d’applications mobiles, intégrateur, référent numérique, datartisan, e-commerçant… et bien d’autres métiers numériques.</p>
+
+  <h2>ERN Narbonne</h2>
+
+  <p>15 participants, résidents d'occitanie, ont été sélectionnés sur la base de leur talent et de leur motivation, pour participer à la formation "Développeur Logiciel" financée par la région.
+  Cette dernière dure 10 mois (dont 2 et demi de stage en entreprise).
+  Un titre professionnel équivalent à BAC +2 peut être obtenu si l'ensemble des compétences sont acquises.</p>
+
+<?php include 'includes/templates/footer.php';?>
+
+</body>
+</html>
