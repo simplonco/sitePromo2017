166860f56fea00520c17a408f21822166826be2e
Nicolas <nvilla@simplon.co>
2017-05-05 10:11:33 +0200

    Ajout du lien vers la page a propos sur l'accueil

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
       </div>
       <div class="divScnd">
         <ul class="menuScnd">
-          <li class="test"><a href="#">Introduction</a></li>
+          <li class="test"><a href="./a_propos.php">Introduction</a></li>
           <li class="test"><a href="./outils.php">Outils</a></li>
           <li class="test"><a href="./projet.php">Nos projets</a></li>
           <li class="test"><a href="./trombi.php">Trombinoscope</a></li>
