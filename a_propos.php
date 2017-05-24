5a7aadb36af0c90d71409d07cfb0c5e439cee4b4
Nicolas <nvilla@simplon.co>
2017-05-24 16:55:20 +0200

    Programme

--- a_propos.php
+++ a_propos.php
@@ -7,27 +7,41 @@
     <link rel="stylesheet" href="./assets/css/trombi.css">
     <link rel="stylesheet" href="./assets/css/footer.css">
     <link rel="stylesheet" href="./assets/css/a_propos.css">
-    <title>Présentation</title>
+    <title>Présentation de la formation</title>
 </head>
 <body id="haut">
       <div class="content">
         <?php include 'includes/templates/header.php';?>
         <div class="apropos">
-        <h1> A propos... </h1>
+        <h1>La formation</h1>
 
-        <h2> SIMPLON</h2>
+        <div class="presta">
+          <div class="simplon">
+            <h2>Dispensée par SIMPLON</h2>
 
-        <p>Entreprise de l’économie sociale et solidaire (agrément ESUS), Simplon.co est un réseau de « fabriques » (écoles) qui propose des formations GRATUITES pour devenir développeur de sites web et d’applications mobiles, intégrateur, référent numérique, datartisan, e-commerçant… et bien d’autres métiers numériques.</p>
+            <p>Entreprise de l’économie sociale et solidaire (agrément ESUS), Simplon.co est un réseau de « fabriques » (écoles) qui propose des formations GRATUITES pour devenir développeur de sites web et d’applications mobiles, intégrateur, référent numérique, datartisan, e-commerçant… et bien d’autres métiers numériques.</p>
+          </div>
 
-        <h2>Ecole Régionale du Numérique</h2>
+          <div>
+            <h2>Financée par l'Ecole Régionale du Numérique</h2>
 
-        <p>15 participants, résidents d'occitanie, ont été sélectionnés sur la base de tests techniques et de leur motivation, pour participer à la formation "Développeur Logiciel" financée par la Région Occitanie.
-        Cette dernière dure 10 mois (dont 2 et demi de stage en entreprise).
-        Un titre professionnel équivalent à BAC +2 peut être obtenu si l'ensemble des compétences sont acquises.</p>
+            <p>15 participants, résidents d'occitanie, ont été sélectionnés sur la base de tests techniques et de leur motivation, pour participer à la formation "Développeur Logiciel" financée par la Région Occitanie.
+            Cette dernière dure 10 mois (dont 2 et demi de stage en entreprise).
+            Un titre professionnel équivalent à BAC +2 peut être obtenu si l'ensemble des compétences sont acquises.</p>
+          </div>
+        </div>
 
-        <p>
-
-        </p>
+        <h2>Le programme</h2>
+          <p>
+            <ul>
+              <li>Module 1 : introduction aux systèmes informatiques et aux cultures numériques</li>
+              <li>Module 2 : Prise en main de l’environnement de développement</li>
+              <li>Module 3 : Développer l’interface d’une application web responsive pour smartphone et tablette</li>
+              <li>Module 4 : Développer des pages web en lien avec une base de données</li>
+              <li>Module 5 : Développer un site à l’aide d’un système de gestion de contenu ou d’un framework</li>
+              <li>Module 6 : Développer un site d’e-commerce</li>
+            </ul>
+          </p>
         </div>
       </div>
         <?php include 'includes/templates/footer.php';?>
