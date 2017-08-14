1a3f2d585ed850ab39612b3bd1f59c27674200d0
atax <ad.joblink@gmail.com>
2017-08-14 15:33:49 +0200

    retouche css

--- lexique/lexique.php
+++ lexique/lexique.php
@@ -11,91 +11,74 @@
 		<header>
 			<h1>Lexique</h1>
 			<nav class="navLexique">
-				<a href="#a">A</a>
-				<a href="#b">B</a>
-				<a href="#c">C</a>
-				<a href="#d">D</a>
-				<a href="#e">E</a>
-				<a href="#f">F</a>
-				<a href="#g">G</a>
-				<a href="#h">H</a>
-				<a href="#i">I</a>
-				<a href="#j">J</a>
-				<a href="#k">K</a>
-				<a href="#l">L</a>
-				<a href="#m">M</a>
-				<a href="#n">N</a>
-				<a href="#o">O</a>
-				<a href="#p">P</a>
-				<a href="#q">Q</a>
-				<a href="#r">R</a>
-				<a href="#s">S</a>
-				<a href="#t">T</a>
-				<a href="#u">U</a>
-				<a href="#v">V</a>
-				<a href="#w">W</a>
-				<a href="#x">X</a>
-				<a href="#y">Y</a>
-				<a href="#z">Z</a>
+				<a href="#a">A</a><a href="#b">B</a><a href="#c">C</a>
+				<a href="#d">D</a><a href="#e">E</a><a href="#f">F</a>
+				<a href="#g">G</a><a href="#h">H</a><a href="#i">I</a>
+				<a href="#j">J</a><a href="#k">K</a><a href="#l">L</a>
+				<a href="#m">M</a><a href="#n">N</a><a href="#o">O</a>
+				<a href="#p">P</a><a href="#q">Q</a><a href="#r">R</a>
+				<a href="#s">S</a><a href="#t">T</a><a href="#u">U</a>
+				<a href="#v">V</a><a href="#w">W</a><a href="#x">X</a>
+				<a href="#y">Y</a><a href="#z">Z</a>
 			</nav>
 		</header>
 		<section id="definitions">
-			<div id="a">A</div>
+			<div id="a" class="puces">A</div>
 				<?php include("./includes articles/apache.php") ?>
 				<?php include("./includes articles/api.php") ?>
 				<?php include("./includes articles/agile.php") ?>
-			<div id="b">B</div>
+			<div id="b" class="puces">B</div>
 				<?php include("./includes articles/bower.php") ?>
-			<div id="c">C</div>
+			<div id="c" class="puces">C</div>
 				<?php include("./includes articles/cms.php") ?>
 				<?php include("./includes articles/crud.php") ?>
 				<?php include("./includes articles/css.php") ?>
-			<div id="d">D</div>
+			<div id="d" class="puces">D</div>
 				<?php include("./includes articles/diagramme_de_classe.php") ?>
 				<?php include("./includes articles/dry.php") ?>
-			<div id="e">E</div>
+			<div id="e" class="puces">E</div>
 				<?php include("./includes articles/ecmascript.php") ?>
 				<?php include("./includes articles/ecma.php") ?>
-			<div id="f">F</div>
+			<div id="f" class="puces">F</div>
 				<?php include("./includes articles/flow_chart.php") ?>
 				<?php include("./includes articles/ftp.php") ?>
-			<div id="g">G</div>
+			<div id="g" class="puces">G</div>
 				<?php include("./includes articles/git.php") ?>
 				<?php include("./includes articles/grunt.php") ?>
 				<?php include("./includes articles/gulp.php") ?>
-			<div id="h">H</div>
+			<div id="h" class="puces">H</div>
 				<?php include("./includes articles/html.php") ?>
-			<div id="i">I</div>
+			<div id="i" class="puces">I</div>
 				<?php include("./includes articles/I_C.php") ?>
-			<div id="j">J</div>
+			<div id="j" class="puces">J</div>
 				<?php include("./includes articles/json.php") ?>
-			<div id="k">K</div>
+			<div id="k" class="puces">K</div>
 				<?php include("./includes articles/kanban.php") ?>
 				<?php include("./includes articles/kiss.php") ?>
-			<div id="l">L</div>
+			<div id="l" class="puces">L</div>
 				<?php include("./includes articles/linux.php") ?>
-			<div id="m">M</div>
+			<div id="m" class="puces">M</div>
 				<?php include("./includes articles/mbaas.php") ?>
 				<?php include("./includes articles/mcd.php") ?>
 				<?php include("./includes articles/mockup.php") ?>
 				<?php include("./includes articles/MLD.php") ?>
 				<?php include("./includes articles/moscow.php") ?>
-			<div id="n">N</div>
+			<div id="n" class="puces">N</div>
 				<?php include("./includes articles/nih.php") ?>
 				<?php include("./includes articles/npm.php") ?>
-			<div id="o">O</div>
-			<div id="p">P</div>
+			<div id="o" class="puces">O</div>
+			<div id="p" class="puces">P</div>
 				<?php include("./includes articles/pdo.php") ?>
 				<?php include("./includes articles/poo_private.php") ?>
 				<?php include("./includes articles/poo_protected.php") ?>
 				<?php include("./includes articles/poo_public.php") ?>
 				<?php include("./includes articles/poo_static.php") ?>
 				<?php include("./includes articles/psr.php") ?>
-			<div id="q">Q</div>
-			<div id="r">R</div>
+			<div id="q" class="puces">Q</div>
+			<div id="r" class="puces">R</div>
 				<?php include("./includes articles/refactoring.php") ?>
 				<?php include("./includes articles/rowe.php") ?>
-			<div id="s">S</div>
+			<div id="s" class="puces">S</div>
 				<?php include("./includes articles/sass.php") ?>
 				<?php include("./includes articles/scrum.php") ?>
 				<?php include("./includes articles/seo.php") ?>
@@ -103,21 +86,23 @@
 				<?php include("./includes articles/solid.php") ?>
 				<?php include("./includes articles/ssh.php") ?>
 				<?php include("./includes articles/stand_up_meeting.php") ?>
-			<div id="t">T</div>
+			<div id="t" class="puces">T</div>
 				<?php include("./includes articles/tdd.php") ?>
 				<?php include("./includes articles/token_d_api.php") ?>
-			<div id="u">U</div>
+			<div id="u" class="puces">U</div>
 				<?php include("./includes articles/use_case.php") ?>
 				<?php include("./includes articles/user_stories.php") ?>
 				<?php include("./includes articles/user_experience.php") ?>
-			<div id="v">V</div>
-			<div id="w">W</div>
+			<div id="v" class="puces">V</div>
+			<div id="w" class="puces">W</div>
 				<?php include("./includes articles/w3c.php") ?>
 				<?php include("./includes articles/wire_frame.php") ?>
-			<div id="x">X</div>
+			<div id="x" class="puces">X</div>
 				<?php include("./includes articles/xml_http_request.php") ?>
-			<div id="x">Y</div>
+			<div id="y" class="puces">Y</div>
 				<?php include("./includes articles/yagni.php") ?>
+			<div id="z" class="puces">Z</div>
 	  </section>
+
 	</body>
 </html>
